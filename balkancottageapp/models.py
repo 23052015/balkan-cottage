73840a6ccd51a957cb8ce31012979ab8838512c7
23052015 <stani.ste13@gmail.com>
2023-04-11 14:09:18 +0000

    add models to models.py

--- balkancottageapp/models.py
+++ balkancottageapp/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Menu(models.Model):
+    dish_name = models.CharField(max_length=130)
+    extra = models.CharField(max_length=130)
+    description = models.CharField(max_length=200)
+    price = models.FloatField()
+    image = CloudinaryField('image', default='')
+    likes = models.ManyToManyField(
+        User, related_name='dish_like', blank=True
+        )
+
+    def __str__(self):
+        return self.dish_name
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+# Reservation model
+
+
+class Reservation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    table = models.IntegerField()
+    reservation_date = models.DateField()
+    reservation_time = models.TimeField()
+
+# Table Model
+
+
+class Table(models.Model):
+    TABLE_CHOICES = (
+        ('4', '4 perosns'),
+        ('8', '8 perosns'),
+        ('10', '10 perosns'),
+    )
+    table_number = models.IntegerField(unique=True)
+    capacity = models.CharField(max_length=2, choices=TABLE_CHOICES)
+    is_available = models.BooleanField(default=True)
+
+# Comments from registered Users
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name='liked_comments')
+
+    def __str__(self):
+        return f'Commented by {self.user.username}'
+
+
--- balkancottageapp/views.py
+++ balkancottageapp/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Reservation, Table
+from datetime import datetime, timedelta
 
-# Create your views here.
