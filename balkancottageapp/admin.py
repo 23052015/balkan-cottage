d479aa56fd084cb2b07a78adf2e46b81f64b2ea2
23052015 <stani.ste13@gmail.com>
2023-04-11 15:58:44 +0000

    install summernotes and add to requirements, add summernote ti installed apps, add summernote to urls and admin.py

--- balkancottageapp/admin.py
+++ balkancottageapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Menu
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Menu)
+class PostAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('content')
